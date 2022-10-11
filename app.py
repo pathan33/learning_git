9fff12089f78a7a9ee0de6e9c18a8d042ec200a2
pathan33 <patandadakhalandar35@gmail.com>
2022-10-12 00:24:44 +0530

    added a flask app code

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("Added the app.py file")
+from flask import Flask
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def welcome():
+    return "Hello World"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
